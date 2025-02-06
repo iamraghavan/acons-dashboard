40826e8988ab3723379eb407962f4f8fd184034a
iam.raghavan <developer.raghavan@gmail.com>
2025-02-06 17:23:31 +0530

    update main entry point and change default port to 3001

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const flash = require('connect-flash');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(helmet({
